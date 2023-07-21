637cb6c1f122819567c742986558b33d78a2c7d8
Samantha England <64668742+EnglandSamanthaa@users.noreply.github.com>
2023-07-20 23:07:55 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -11,6 +11,71 @@ class ToDoTask {
 
 const toDoItems = [];
 
+function updateToDoList(){
+    while (listContainer.firstChild) {
+        listContainer.removeChild(listContainer.firstChild);
+    }
+
+    toDoItems.forEach((item, index) => {
+        addSingleItem(item);
+    });
+}
+
+function addSingleItem(item) {
+    const li = document.createElement('li');
+    const unchecked = document.createElement("img");
+    unchecked.src = "IMG/icons8-button-32.png";
+    const checked = document.createElement("img");
+    checked.src = "IMG/icons8-checked-checkbox-50.png";
+    const trashCan = document.createElement("img");
+    trashCan.src = "IMG/icons8-trash-can-32.png";
+
+    unchecked.style.marginRight = "20px";
+    unchecked.style.width = "50px";
+    unchecked.style.height = "50px";
+
+    checked.style.marginRight = "20px";
+    checked.style.width = "50px";
+    checked.style.height = "50px";
+
+    trashCan.style.width = "30px";
+    trashCan.style.height= "30px";
+
+    const text = document.createTextNode(item.task);
+    const textSpan = document.createElement('span');
+    textSpan.appendChild(text);
+    
+    li.appendChild(unchecked);
+    li.appendChild(textSpan);
+    li.appendChild(trashCan);
+
+
+    textSpan.style.width = "450px";
+
+    listContainer.appendChild(li);
+
+    trashCan.addEventListener("click", (event => {
+        listContainer.removeChild(li);
+        toDoItems.splice(toDoItems.indexOf(item), 1); // Remove the item from the toDoItems array
+    }));
+
+    li.addEventListener("click", (event => {
+        if (li.contains(unchecked)) {
+            li.replaceChild(checked, unchecked);
+            item.completed = true;
+        } else {
+            li.replaceChild(unchecked, checked);
+            item.completed = false;
+        }
+
+        if (item.completed === true) {
+            li.style.textDecoration = 'line-through';
+        } else {
+            li.style.textDecoration = 'none';
+        }
+    }));
+
+}
 function addItem(){
     const task = inputBox.value.trim();
     if (task === '') return;
@@ -24,51 +89,17 @@ function addItem(){
     updateToDoList();
 }
 
-function updateToDoList(){
-    listContainer.innerHTML = '';
-
-    toDoItems.forEach((item, index) => {
-        const li = document.createElement('li');
-        const unchecked = document.createElement("img");
-            unchecked.src = "IMG/icons8-button-32.png";
-        const checked = document.createElement("img");
-            checked.src = "IMG/icons8-checked-checkbox-50.png"
-
-        const text = document.createTextNode(item.task);
-        li.appendChild(unchecked);
-        li.appendChild(text);
-        listContainer.appendChild(li);
-
-        unchecked.style.marginRight = "20px";
-        unchecked.style.width = "50px";
-
-        checked.style.marginRight = "20px";
-        checked.style.width = "50px";
-
-
-
-
-        li.addEventListener("click", (event => {
-            if (li.contains(unchecked)){
-                li.replaceChild(checked, unchecked);
-                item.completed = true;
-            } else {
-                li.replaceChild(unchecked, checked);
-                item.completed = false;
-            }
-
-            if (item.completed === true) {
-                li.style.textDecoration = 'line-through';
-            } else {
-                li.style.textDecoration = 'none';
-            }
-        }))
-    })
-}
-
-
 addButton.addEventListener("click", (event => {
-    addItem();
-    console.log(toDoItems)
-}))
+    addItem();    
+}));
 
+inputBox.addEventListener("keydown", (event =>{ 
+    if (event.keyCode === 13) {
+        addItem();
+    }
+}));
+
+window.addEventListener("load", (e => {
+    updateToDoList()
+    inputBox.focus();
+}));
